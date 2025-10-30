4a2fcb13856c0552fd82ed910f70680c4d09d343
bailey38 <baileyhall38@gmail.com>
2025-10-30 22:25:54 +1100

    Update GREEN color value in COLORS constant
    Changed the GREEN color in the COLORS constant from '#2FB52F' to '#00ff00ff' to ensure consistency or meet new design requirements.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 const COLORS = {
   CLEAR: "#000000",
   RED: "#FF0000",
-  GREEN: "#2FB52F",
+  GREEN: "#00ff00ff",
   YELLOW: "#FFFF00",
   YELLOW_FLASH: "#FFFF00",
   WHITE: "#FFFFFF",
